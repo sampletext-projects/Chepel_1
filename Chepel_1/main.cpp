f018cbe2a1b888b6aecb0382e2aec893a9f2124b
sampletext32 <sampletext32@bk.ru>
2020-04-09 22:09:30 +0300

    Add project

---
+++ Chepel_1/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double o1, o2;
+	double r;
+
+	char op;
+	char k;
+
+	do
+	{
+		cout << "First: ";
+		cin >> o1;
+		cout << "Operation: ";
+		cin >> op;
+		cout << "Second: ";
+		cin >> o2;
+		cout << "Result: ";
+		switch (op)
+		{
+		case '+':
+			r = o1 + o2;
+			break;
+		case '-':
+			r = o1 - o2;
+			break;
+		case '*':
+			r = o1 * o2;
+			break;
+		case '/':
+			r = o1 / o2;
+			break;
+		default: ;
+		}
+		cout << r;
+		cout << "\nRepeat?: ";
+		cin >> k;
+	}
+	while (k == '1');
+
+
+	return 0;
+}
